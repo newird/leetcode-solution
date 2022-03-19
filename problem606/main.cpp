64e059cc86c844bc9bb01e9e306abcc3e8b1321a
newird <zhangce9944@163.com>
2022-03-19 18:13:32 +0800

    problem 606 time 12ms memory 22.7mb 复习了树的非递归先序遍历，但是好像不太好写，遂放弃，使用递归实现，主要是注意括号的匹配

---
+++ problem606/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include<string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    string tree2str(TreeNode *root) {
+        if (root == nullptr) return "";
+        string ans;
+        Preoder(root, ans);
+        return ans;
+
+    }
+
+    void Preoder(TreeNode *root, string &s) {
+        if (root == nullptr) s += "";
+        else {
+            s+= to_string(root->val);
+            if (root->left || root->right) {
+                s += "(";
+                if (root->left) {
+                    Preoder(root->left, s);
+                }
+                s += ")";
+
+                if (root->right) {
+                    s += "(";
+                    Preoder(root->right, s);
+                    s += ")";
+                }
+            }
+        }
+    }
+};
+
+int main() {
+    TreeNode *right = new TreeNode(3, nullptr, nullptr);
+    TreeNode *left = new TreeNode(2, nullptr, right);
+    TreeNode *root = new TreeNode(1, left, nullptr);
+    Solution s;
+    cout << s.tree2str(root);
+    return 0;
+}
