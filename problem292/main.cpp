cf1bcf8dcfcc6ca37d73c0fb9ceada24ef03cafd
newird <zhangce9944@163.com>
2022-03-29 11:15:07 +0800

    problem 292 time 0ms memory 5.8mb

---
+++ problem292/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4;
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
