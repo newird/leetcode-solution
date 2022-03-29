765d248d6dbc51ab35d33ee7f6ace74437ad9bcb
newird <zhangce9944@163.com>
2022-03-29 11:38:45 +0800

    problem 877 time 0ms memory 7.3mb 总是为true，为什么？

---
+++ problem877/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+
+    bool stoneGame(vector<int> &piles) {
+        return true;
+    }
+
+
+};
+
+int main() {
+    Solution solution;
+    vector<int> piles{1, 2, 3, 4};
+    cout << solution.stoneGame(piles);
+    return 0;
+}
